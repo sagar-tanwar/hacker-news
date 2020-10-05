5c96698000636a5bf493f9d8b2384a2da802bc39
Sagar Tanwar <thevisionary_mind@outlook.com>
2020-10-06 03:47:22 +0530

    Splits code at Route lever

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import TopPosts from './components/TopPosts'
-import NewPosts from './components/NewPosts'
-import Post from './components/Post'
-import User from './components/User'
 import Nav from './components/Nav'
+import Loading from './components/Loading'
 import {ThemeProvider} from './contexts/theme'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+const TopPosts = React.lazy(() => import('./components/TopPosts'))
+const NewPosts = React.lazy(() => import('./components/NewPosts'))
+const User = React.lazy(() => import('./components/User'))
+const Post = React.lazy(() => import('./components/Post'))
 
 class App extends React.Component {
   state = {
@@ -25,10 +27,12 @@ class App extends React.Component {
         <ThemeProvider value={this.state}>
           <div className={this.state.theme}>
             <Nav />
-            <Route path='/' exact component={TopPosts} />
-            <Route path='/new' component={NewPosts} />
-            <Route path='/user' component={User} />
-            <Route path='/post' component={Post} />
+            <React.Suspense fallback={<Loading />}>
+              <Route path='/' exact component={TopPosts} />
+              <Route path='/new' component={NewPosts} />
+              <Route path='/user' component={User} />
+              <Route path='/post' component={Post} />
+            </React.Suspense>
           </div>
         </ThemeProvider>
       </Router>
